3d06ef28a06fe0a30e7012ab5b8780a2a1f5c985
Kieran Caraway <kierancaraway20@yahoo.com>
2024-09-14 03:30:13 -0500

    latest update
    latest update

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-//? Created a variable for the textbox.
+//? Created a variable for the textbox(input).
 let userInput = document.getElementById("taskInput");
+
+//* Created a variable for the add task button
 let addTaskButton = document.querySelector("#addTaskBtn");
-let taskDisplay = document.querySelectorAll(".display p");
+
+// let listItems = document.getElementById("")
+// let firstItemText = document.createElement("p")
+// listItems.append(firstItemText);
+// firstItemText.innerHTML = userInput.value
 
 
-//* Checks the input of the textbox
-userInput.addEventListener("input", () => {
-    let input = userInput.value
-    console.log(input)
-})
+let taskList = document.getElementById("taskList");
 
-//todo Display the tasks input by the user to the task list below it
-//todo Make it to where a new list item is added every time add task button is clicked
-addTaskButton.addEventListener("click", () => {
-    for (let i = 0; i < taskDisplay.length; i++) {
-        let input = userInput.value;
-        //* template for creating task lists.
-        taskDisplay[i].innerHTML = input;
-    }
+// //* Checks the input of the textbox
+// userInput.addEventListener("input", () => {
+//     let input = userInput.value
+//     console.log(input)
+// })
+
+function addNewTask() {
+    //* task list items being created
+    //* adding class names to newly created li items
+    //* append new li elements to the existing UL
+    let newListItem = document.createElement("li");
+    newListItem.classList.add("list-group-item")
+    newListItem.classList.add("task-item")
+    newListItem.classList.add("d-flex")
+    taskList.append(newListItem);
+    
+    //* creating new input element and changing the type for the new input element.
+    //* adding class names to the newly created input elements
+    //* adding new input to the new li items
+    newTaskTextbox = document.createElement("input");
+    newTaskTextbox.type = "checkbox";
+    newTaskTextbox.classList.add("form-check-input");
+    newListItem.append(newTaskTextbox);
+    
+    //* creating new p elements
+    //* appending those p elements to the lists
+    let newText = document.createElement("p");
+    newListItem.append(newText);
+    // listItems.innerHTML = userInput.value;
+    newText.innerHTML = userInput.value;
+
     clearInput()
-});
+}
 
 function clearInput() {
     userInput.value = "";
-}
+};
+
+addTaskButton.addEventListener("click", addNewTask);
+
+
---
+++ pokemon.js
@@ -0,0 +1,28 @@
+async function fetchPokeData() {
+    let url = `https://pokeapi.co/api/v2/pokemon/mew`;
+
+    //* Activates fetching 
+    await fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.stats[4].stat.name);
+            displayPokemon(data.sprites.front_default);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+};
+
+fetchPokeData();
+
+ function displayPokemon(pokeImg) {
+    let characterTraits = document.querySelector(".characterTraits");
+    let imgSrc = document.createElement("img");
+    imgSrc.src = pokeImg;
+    imgSrc.width = 400;
+    imgSrc.height = 400;
+    characterTraits.append(imgSrc);
+};
+
+
+
